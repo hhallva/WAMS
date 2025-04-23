4c008ee4250d992855806268cb3420038fb08357
Вячеслав <slavvva05@gmail.com>
2025-04-23 23:02:43 +0300

    Создал шаблон для контроллера зарплаты

---
+++ ServiceLayer/ServiceLayer/DTOs/SalaryDto.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceLayer.DTOs
+{
+    public class SalaryDto
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int HoursCount { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public bool IsPaid { get; set; }
+    }
+}
---
+++ ServiceLayer/WebApi/Controllers/SalariesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer.DataContexts;
+using ServiceLayer.DTOs;
+using ServiceLayer.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [SwaggerTag("Управление зарплатами")]
+    public class SalariesController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("{userId}/Current")]
+        #region Докуметация
+        [SwaggerOperation(Summary = "Получение текущей зарплаты.", Description = "Метод для получения текущей зарплаты сотрудника из БД.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Успешное получение зарплаты. Возврат объекта зарплаты.", Type = typeof(SalaryDto))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Зарплата не найдена. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
+        #endregion
+        public async Task<ActionResult<SalaryDto>> GetCurrentSalaryAsync(int userId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{userId}/Month")]
+        #region Докуметация
+        [SwaggerOperation(Summary = "Получение зарплаты за месяц.", Description = "Метод для получения зарплаты сотрудника за месяц из БД.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Успешное получение зарплаты. Возврат объекта зарплаты.", Type = typeof(SalaryDto))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Зарплата не найдена. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
+        #endregion
+        public async Task<ActionResult<SalaryDto>> GetSalaryByMonthAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{userId}")]
+        #region Докуметация
+        [SwaggerOperation(Summary = "Получение зарплаты сотрудника.", Description = "Метод для получения зарплаты сотрудника из БД.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Успешное получение зарплаты. Возврат объекта зарплаты.", Type = typeof(SalaryDto))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Зарплата не найдена. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
+        #endregion
+        public async Task<ActionResult<SalaryDto>> GetSalaryByUserAsync()
+        {
+            return Ok();
+        }
+    }
+}

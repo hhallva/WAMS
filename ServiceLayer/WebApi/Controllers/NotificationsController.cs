6f74ca07a251e09874305be7cb71f4603ab840af
Вячеслав <slavvva05@gmail.com>
2025-04-23 23:15:22 +0300

    Реализовал контроллер уведомлений

--- ServiceLayer/WebApi/Controllers/NotificationsController.cs
+++ ServiceLayer/WebApi/Controllers/NotificationsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ServiceLayer.DataContexts;
 using ServiceLayer.DTOs;
+using ServiceLayer.Models;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace WebApi.Controllers
@@ -17,7 +18,7 @@ namespace WebApi.Controllers
         [SwaggerResponse(StatusCodes.Status200OK, "Успешное получение уведомлений. Возврат списка уведомлений.", Type = typeof(IEnumerable<NotificationDto>))]
         [SwaggerResponse(StatusCodes.Status404NotFound, "Уведомления не найдены. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
         #endregion
-        public async Task<ActionResult<IEnumerable<NotificationDto>>> GetNotificationsAsync(int userId)
+        public async Task<ActionResult<IEnumerable<NotificationDto>>> GetNotificationsAsync([SwaggerParameter("Id пользователя у которого хотим получить уведомления", Required = true)] int userId)
         {
             var notifications = await context.Notifications
                 .Where(n => n.UserId == userId)
@@ -37,7 +38,7 @@ namespace WebApi.Controllers
         [SwaggerResponse(StatusCodes.Status200OK, "Успешное получение непрочитанных уведомлений. Возврат списка уведомлений.", Type = typeof(IEnumerable<NotificationDto>))]
         [SwaggerResponse(StatusCodes.Status404NotFound, "Уведомления не найдены. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
         #endregion
-        public async Task<ActionResult<IEnumerable<NotificationDto>>> GetNoReadNotificationsAsync(int userId)
+        public async Task<ActionResult<IEnumerable<NotificationDto>>> GetNoReadNotificationsAsync([SwaggerParameter("Id пользователя у которого хотим получить уведомления", Required = true)] int userId)
         {
             var notifications = await context.Notifications
                 .Where(n => n.UserId == userId)
@@ -50,6 +51,31 @@ namespace WebApi.Controllers
             var notificationsDtos = notifications.Select(n => n.ToDto()).ToList();
 
             return Ok(notificationsDtos);
+        }
+
+        [HttpPatch("{id}/Read")]
+        #region Докуметация
+        [SwaggerOperation(Summary = "Отметить уведомление прочитанным.", Description = "Метод для прочтения уведомления сотрудника из БД.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Успешное прочтение уведомления. Возврат прочетнного уведомления.", Type = typeof(NotificationDto))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Уведомление не найдено. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
+        #endregion
+        public async Task<ActionResult<NotificationDto>> PatchNotificationsAsync([SwaggerParameter("Id уведомления которое хотим отметить прочитанным", Required = true)] int id)
+        {
+            var notification = await context.Notifications.FindAsync(id);
+
+            if (notification == null)
+                return NotFound(new ApiErrorDto("Уведомление не найдено", 2001));
+
+            if (notification.IsRead == true)
+                return NotFound(new ApiErrorDto("Уведомления уже прочитано", 2001));
+
+            notification.IsRead = true;
+            context.Notifications.Update(notification);
+            await context.SaveChangesAsync();
+
+            var notificationDto = notification.ToDto();
+
+            return Ok(notificationDto);
         }
     }
 }

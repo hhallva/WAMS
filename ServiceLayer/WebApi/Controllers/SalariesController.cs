74740e1d8736f1c72996d68752fee5794da5eb79
Вячеслав <slavvva05@gmail.com>
2025-04-26 19:03:27 +0300

    Написал несколько методов для получения ЗП

--- ServiceLayer/WebApi/Controllers/SalariesController.cs
+++ ServiceLayer/WebApi/Controllers/SalariesController.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using ServiceLayer.DataContexts;
 using ServiceLayer.DTOs;
-using ServiceLayer.Models;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace WebApi.Controllers
@@ -20,7 +19,15 @@ namespace WebApi.Controllers
         #endregion
         public async Task<ActionResult<SalaryDto>> GetCurrentSalaryAsync([SwaggerParameter("Id пользователя у которого хотим получить зарплату", Required = true)] int userId)
         {
-            return Ok();
+            var salary = await context.Salaries
+                .SingleOrDefaultAsync(s => s.EndDate > DateTime.Now);
+
+            if (salary == null)
+                return NotFound(new ApiErrorDto("Текущая зарплата не найдена",2001));
+
+            var salaryDto = salary.ToDto();
+
+            return Ok(salaryDto);   
         }
 
         [HttpGet("{userId}/Month")]
@@ -41,6 +48,18 @@ namespace WebApi.Controllers
         [SwaggerResponse(StatusCodes.Status404NotFound, "Зарплата не найдена. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
         #endregion
         public async Task<ActionResult<SalaryDto>> GetSalaryByUserAsync([SwaggerParameter("Id пользователя у которого хотим получить зарплату", Required = true)] int userId)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<SalaryDto>> PatchSalaryAsync(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{userId}")]
+        public async Task<ActionResult<SalaryDto>> PostSalaryAsync(int userId)
         {
             return Ok();
         }

5d84b0f20947822cbb914fe433ed2679e70ed470
Вячеслав <slavvva05@gmail.com>
2025-04-26 19:03:07 +0300

    Создание DTO для зарплаты

---
+++ ServiceLayer/ServiceLayer/DTOs/Salary/SalaryMapper.cs
@@ -0,0 +1,24 @@
+﻿using ServiceLayer.Models;
+
+namespace ServiceLayer.DTOs
+{
+    public static class SalaryMapper
+    {
+        public static SalaryDto ToDto(this Salary salary)
+        {
+            if (salary == null)
+                throw new ArgumentNullException(nameof(salary));
+
+            return new SalaryDto
+            {
+                Id = salary.Id,
+                UserId = salary.UserId,
+                StartDate = salary.StartDate,
+                EndDate = salary.EndDate,
+                HoursCount = salary.HoursCount,
+                Amount = salary.Amount,
+                IsPaid = salary.IsPaid,
+            };
+        }
+    }
+}
